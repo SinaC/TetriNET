6892e941834c3ed5eb55ee91267de55bbd297d45
SinaC <heymbeeck.joel@gmail.com>
2013-08-20 20:48:44 +0200

    IPAddress serialization POC
    TODO: move to POC

--- TetriNET.Client/Program.cs
+++ TetriNET.Client/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
-namespace TetriNET.Client.Console
+namespace TetriNET.Client
 {
-    class Program
+    public class Program
     {
+        public class Test : IXmlSerializable
+        {
+            public IPAddress Address { get; set; }
+            public XmlSchema GetSchema()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void ReadXml(XmlReader reader)
+            {
+                string addr = reader.GetAttribute("Address");
+                if (!String.IsNullOrEmpty(addr))
+                    Address = IPAddress.Parse(addr);
+            }
+
+            public void WriteXml(XmlWriter writer)
+            {
+                writer.WriteAttributeString("Address", Address.ToString());
+            }
+        }
+
         static void Main(string[] args)
         {
+            Test test = new Test
+            {
+                Address = IPAddress.Parse("192.168.1.1")
+            };
+
+            XmlSerializer xs = new XmlSerializer(typeof(Test));
+            using (StreamWriter wr = new StreamWriter(@"d:\temp\ipaddress.xml"))
+            {
+                xs.Serialize(wr, test);
+            }
+
+            Test test2;
+            using (StreamReader sr = new StreamReader(@"d:\temp\ipaddress.xml"))
+            {
+                test2 = (Test)xs.Deserialize(sr);
+            }
+
             //string baseAddress = "net.tcp://localhost:8765/TetriNET";
             string baseAddress = ConfigurationManager.AppSettings["address"];
             //SimpleTetriNETProxyManager proxyManager = new SimpleTetriNETProxyManager(baseAddress);
