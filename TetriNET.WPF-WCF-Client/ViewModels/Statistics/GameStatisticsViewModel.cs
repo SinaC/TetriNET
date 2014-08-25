89500b97a1fb2d97b56c87ff7ac844b9f2bd795b
SinaC <heymbeeck.joel@gmail.com>
2014-08-25 16:58:08 +0200

    Backup : DONT GET THIS VERSION

--- TetriNET.WPF-WCF-Client/ViewModels/Statistics/GameStatisticsViewModel.cs
+++ TetriNET.WPF-WCF-Client/ViewModels/Statistics/GameStatisticsViewModel.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using TetriNET.Client.Interfaces;
 using TetriNET.Common.DataContracts;
 using TetriNET.Common.Helpers;
 using TetriNET.WPF_WCF_Client.DynamicGrid;
+using TetriNET.WPF_WCF_Client.Helpers;
 
 namespace TetriNET.WPF_WCF_Client.ViewModels.Statistics
 {
-    public class SpecialRow : DynamicRow
+    public class SpecialStatisticsRow : DynamicRow
     {
         private Specials _special;
         public Specials Special
@@ -27,14 +29,28 @@ namespace TetriNET.WPF_WCF_Client.ViewModels.Statistics
 
     public class GameStatisticsViewModel : ViewModelBase
     {
-        private GameStatistics _gameStatistics;
-        public GameStatistics GameStatistics
+        private const int HistorySize = 10;
+
+        private ObservableCollection<GameStatistics> _gameStatisticsHistory;
+        public ObservableCollection<GameStatistics> GameStatisticsHistory
         {
-            get { return _gameStatistics; }
+            get { return _gameStatisticsHistory; }
+            set { Set(() => GameStatisticsHistory, ref _gameStatisticsHistory, value); }
+        }
+
+        public bool IsHistoryVisible { get { return GameStatisticsHistory.Count > 0; } }
+
+        private GameStatistics _selectedGameStatistics;
+        public GameStatistics SelectedGameStatistics
+        {
+            get { return _selectedGameStatistics; }
             set
             {
-                if (Set(() => GameStatistics, ref _gameStatistics, value))
+                if (Set(() => SelectedGameStatistics, ref _selectedGameStatistics, value))
                 {
+                    InitializePlayerList();
+                    InitializeSpecialsGrid();
+
                     OnPropertyChanged("First3Players");
                     OnPropertyChanged("Next3Players");
                     OnPropertyChanged("IsNextPlayersNeeded");
@@ -45,22 +61,22 @@ namespace TetriNET.WPF_WCF_Client.ViewModels.Statistics
 
         public List<GameStatisticsByPlayer> First3Players
         {
-            get { return _gameStatistics == null ? Enumerable.Empty<GameStatisticsByPlayer>().ToList() : _gameStatistics.Players.Take(3).ToList(); }
+            get { return _selectedGameStatistics == null ? Enumerable.Empty<GameStatisticsByPlayer>().ToList() : _selectedGameStatistics.Players.Take(3).ToList(); }
         }
 
         public List<GameStatisticsByPlayer> Next3Players
         {
-            get { return _gameStatistics == null ? Enumerable.Empty<GameStatisticsByPlayer>().ToList() : _gameStatistics.Players.Skip(3).Take(3).ToList(); }
+            get { return _selectedGameStatistics == null ? Enumerable.Empty<GameStatisticsByPlayer>().ToList() : _selectedGameStatistics.Players.Skip(3).Take(3).ToList(); }
         }
 
         public bool IsNextPlayersNeeded
         {
-            get { return _gameStatistics != null && _gameStatistics.Players.Count > 3; }
+            get { return _selectedGameStatistics != null && _selectedGameStatistics.Players.Count > 3; }
         }
 
         public double MatchTime // In seconds
         {
-            get { return _gameStatistics == null ? 0 : (_gameStatistics.GameFinished - _gameStatistics.GameStarted).TotalSeconds; }
+            get { return _selectedGameStatistics == null ? 0 : (_selectedGameStatistics.GameFinished - _selectedGameStatistics.GameStarted).TotalSeconds; }
         }
 
         private List<string> _playerList;
@@ -81,15 +97,15 @@ namespace TetriNET.WPF_WCF_Client.ViewModels.Statistics
             }
         }
 
-        private DynamicGrid<SpecialRow, DynamicColumn> _specialsFromSelectedPlayerGrid; // Columns: player  Rows: special
-        public DynamicGrid<SpecialRow, DynamicColumn> SpecialsFromSelectedPlayerGrid
+        private DynamicGrid<SpecialStatisticsRow, DynamicColumn> _specialsFromSelectedPlayerGrid; // Columns: player  Rows: special
+        public DynamicGrid<SpecialStatisticsRow, DynamicColumn> SpecialsFromSelectedPlayerGrid
         {
             get { return _specialsFromSelectedPlayerGrid; }
             set { Set(() => SpecialsFromSelectedPlayerGrid, ref _specialsFromSelectedPlayerGrid, value); }
         }
 
-        private DynamicGrid<SpecialRow, DynamicColumn> _specialsToSelectedPlayerGrid; // Columns: player  Rows: special
-        public DynamicGrid<SpecialRow, DynamicColumn> SpecialsToSelectedPlayerGrid
+        private DynamicGrid<SpecialStatisticsRow, DynamicColumn> _specialsToSelectedPlayerGrid; // Columns: player  Rows: special
+        public DynamicGrid<SpecialStatisticsRow, DynamicColumn> SpecialsToSelectedPlayerGrid
         {
             get { return _specialsToSelectedPlayerGrid; }
             set { Set(() => SpecialsToSelectedPlayerGrid, ref _specialsToSelectedPlayerGrid, value); }
@@ -97,7 +113,8 @@ namespace TetriNET.WPF_WCF_Client.ViewModels.Statistics
 
         public GameStatisticsViewModel()
         {
-            //GameStatistics = new GameStatistics
+            GameStatisticsHistory = new ObservableCollection<GameStatistics>();
+            //SelectedGameStatistics = new SelectedGameStatistics
             //{
             //    GameStarted = DateTime.Now.AddMinutes(-10),
             //    GameFinished = DateTime.Now.AddMinutes(-5),
@@ -174,17 +191,39 @@ namespace TetriNET.WPF_WCF_Client.ViewModels.Statistics
         private void OnGameFinished(GameStatistics statistics)
         {
             SelectedPlayer = null; // reset
-            GameStatistics = statistics;
-
-            PlayerList = GameStatistics.Players.Select(x => x.PlayerName).ToList();
-            SelectedPlayer = PlayerList.FirstOrDefault(x => x == Client.Name);
+            ExecuteOnUIThread.Invoke(() => AddStatistics(statistics));
+            SelectedGameStatistics = statistics;
         }
 
         #endregion
 
+        private void AddStatistics(GameStatistics statistics)
+        {
+            // Remove oldest entry if no place left
+            if (GameStatisticsHistory.Count >= HistorySize)
+                GameStatisticsHistory.RemoveAt(HistorySize-1);
+            // Add entry
+            GameStatisticsHistory.Insert(0, statistics);
+            OnPropertyChanged("IsHistoryVisible");
+        }
+
+        private void InitializePlayerList()
+        {
+            if (SelectedGameStatistics != null && SelectedGameStatistics.Players != null)
+            {
+                PlayerList = SelectedGameStatistics.Players.Select(x => x.PlayerName).ToList();
+                SelectedPlayer = PlayerList.FirstOrDefault(x => x == Client.Name);
+            }
+            else
+            {
+                PlayerList = null;
+                SelectedPlayer = null;
+            }
+        }
+
         private void InitializeSpecialsGrid()
         {
-            if (SelectedPlayer == null || GameStatistics == null || GameStatistics.Players == null)
+            if (SelectedPlayer == null || SelectedGameStatistics == null || SelectedGameStatistics.Players == null)
             {
                 SpecialsFromSelectedPlayerGrid = null;
                 SpecialsToSelectedPlayerGrid = null;
@@ -199,7 +238,7 @@ namespace TetriNET.WPF_WCF_Client.ViewModels.Statistics
         private void InitializeFromSelectedPlayer()
         {
             // From selected player
-            GameStatisticsByPlayer byPlayer = GameStatistics.Players.FirstOrDefault(x => x.PlayerName == SelectedPlayer);
+            GameStatisticsByPlayer byPlayer = SelectedGameStatistics.Players.FirstOrDefault(x => x.PlayerName == SelectedPlayer);
             if (byPlayer == null || byPlayer.SpecialsUsed == null)
                 SpecialsFromSelectedPlayerGrid = null;
             else
@@ -211,10 +250,10 @@ namespace TetriNET.WPF_WCF_Client.ViewModels.Statistics
                     Type = typeof(string),
                     IsReadOnly = true,
                 }).ToList();
-                List<SpecialRow> rows = new List<SpecialRow>();
+                List<SpecialStatisticsRow> rows = new List<SpecialStatisticsRow>();
                 foreach (KeyValuePair<Specials, Dictionary<string, int>> specials in byPlayer.SpecialsUsed.OrderBy(kv => kv.Key))
                 {
-                    SpecialRow row = new SpecialRow
+                    SpecialStatisticsRow row = new SpecialStatisticsRow
                     {
                         Special = specials.Key
                     };
@@ -226,27 +265,27 @@ namespace TetriNET.WPF_WCF_Client.ViewModels.Statistics
                     }
                     rows.Add(row);
                 }
-                SpecialsFromSelectedPlayerGrid = new DynamicGrid<SpecialRow, DynamicColumn>(rows, columns);
+                SpecialsFromSelectedPlayerGrid = new DynamicGrid<SpecialStatisticsRow, DynamicColumn>(rows, columns);
             }
         }
 
         private void InitializeToSelectedPlayer()
         {
-            List<DynamicColumn> columns = GameStatistics.Players.Select(x => new DynamicColumn
+            List<DynamicColumn> columns = SelectedGameStatistics.Players.Select(x => new DynamicColumn
                 {
                     Name = x.PlayerName,
                     DisplayName = x.PlayerName,
                     Type = typeof(int),
                     IsReadOnly = true,
                 }).ToList();
-            List<Specials> specials = GameStatistics.Players.Where(p => p.SpecialsUsed != null).SelectMany(p => p.SpecialsUsed.Select(kv => kv.Key)).Distinct().ToList();
-            List<SpecialRow> rows = specials.Select(x => new SpecialRow
+            List<Specials> specials = SelectedGameStatistics.Players.Where(p => p.SpecialsUsed != null).SelectMany(p => p.SpecialsUsed.Select(kv => kv.Key)).Distinct().ToList();
+            List<SpecialStatisticsRow> rows = specials.Select(x => new SpecialStatisticsRow
                 {
                     Special = x
                 }).ToList();
-            foreach (GameStatisticsByPlayer statsByPlayer in GameStatistics.Players)
+            foreach (GameStatisticsByPlayer statsByPlayer in SelectedGameStatistics.Players)
             {
-                foreach (SpecialRow row in rows)
+                foreach (SpecialStatisticsRow row in rows)
                 {
                     int value = 0;
                     if (statsByPlayer.SpecialsUsed != null && statsByPlayer.SpecialsUsed.ContainsKey(row.Special))
@@ -257,7 +296,7 @@ namespace TetriNET.WPF_WCF_Client.ViewModels.Statistics
                     row.TryAddProperty(statsByPlayer.PlayerName, value);
                 }
             }
-            SpecialsToSelectedPlayerGrid = new DynamicGrid<SpecialRow, DynamicColumn>(rows, columns);
+            SpecialsToSelectedPlayerGrid = new DynamicGrid<SpecialStatisticsRow, DynamicColumn>(rows, columns);
         }
     }
 
@@ -265,62 +304,77 @@ namespace TetriNET.WPF_WCF_Client.ViewModels.Statistics
     {
         public GameStatisticsViewModelDesignData()
         {
-            GameStatistics = new GameStatistics
+            GameStatisticsHistory = new ObservableCollection<GameStatistics>
                 {
-                    GameStarted = DateTime.Now.AddMinutes(-10),
-                    GameFinished = DateTime.Now.AddMinutes(-5),
-                    Players = new List<GameStatisticsByPlayer>
+                    new GameStatistics
                         {
-                            new GameStatisticsByPlayer
+                            GameStarted = DateTime.Now.AddMinutes(-10),
+                            GameFinished = DateTime.Now.AddMinutes(-5),
+                            Players = new List<GameStatisticsByPlayer>
                                 {
-                                    PlayerName = "Player1",
-                                    SingleCount = 4,
-                                    DoubleCount = 3,
-                                    TripleCount = 2,
-                                    TetrisCount = 1,
-                                },
-                                new GameStatisticsByPlayer
-                                {
-                                    PlayerName = "Player2",
-                                    SingleCount = 8,
-                                    DoubleCount = 6,
-                                    TripleCount = 4,
-                                    TetrisCount = 0,
-                                },
-                                new GameStatisticsByPlayer
-                                {
-                                    PlayerName = "Player3",
-                                    SingleCount = 1,
-                                    DoubleCount = 1,
-                                    TripleCount = 0,
-                                    TetrisCount = 0,
-                                },
-                                new GameStatisticsByPlayer
-                                {
-                                    PlayerName = "Player4",
-                                    SingleCount = 0,
-                                    DoubleCount = 0,
-                                    TripleCount = 0,
-                                    TetrisCount = 1,
-                                },
-                                new GameStatisticsByPlayer
-                                {
-                                    PlayerName = "Player5",
-                                    SingleCount = 12,
-                                    DoubleCount = 7,
-                                    TripleCount = 0,
-                                    TetrisCount = 0,
-                                },
-                                //new GameStatisticsByPlayer
-                                //{
-                                //    PlayerName = "Player6",
-                                //    SingleCount = 20,
-                                //    DoubleCount = 17,
-                                //    TripleCount = 5,
-                                //    TetrisCount = 2,
-                                //},
+                                    new GameStatisticsByPlayer
+                                        {
+                                            PlayerName = "Player1",
+                                            SingleCount = 4,
+                                            DoubleCount = 3,
+                                            TripleCount = 2,
+                                            TetrisCount = 1,
+                                        },
+                                    new GameStatisticsByPlayer
+                                        {
+                                            PlayerName = "Player2",
+                                            SingleCount = 8,
+                                            DoubleCount = 6,
+                                            TripleCount = 4,
+                                            TetrisCount = 0,
+                                        },
+                                    new GameStatisticsByPlayer
+                                        {
+                                            PlayerName = "Player3",
+                                            SingleCount = 1,
+                                            DoubleCount = 1,
+                                            TripleCount = 0,
+                                            TetrisCount = 0,
+                                        },
+                                    new GameStatisticsByPlayer
+                                        {
+                                            PlayerName = "Player4",
+                                            SingleCount = 0,
+                                            DoubleCount = 0,
+                                            TripleCount = 0,
+                                            TetrisCount = 1,
+                                        },
+                                    new GameStatisticsByPlayer
+                                        {
+                                            PlayerName = "Player5",
+                                            SingleCount = 12,
+                                            DoubleCount = 7,
+                                            TripleCount = 0,
+                                            TetrisCount = 0,
+                                        },
+                                    //new GameStatisticsByPlayer
+                                    //{
+                                    //    PlayerName = "Player6",
+                                    //    SingleCount = 20,
+                                    //    DoubleCount = 17,
+                                    //    TripleCount = 5,
+                                    //    TetrisCount = 2,
+                                    //},
+                                }
+                        },
+                        new GameStatistics
+                        {
+                            GameStarted = DateTime.Now.AddMinutes(-20),
+                            GameFinished = DateTime.Now.AddMinutes(-18),
+                        },
+                        new GameStatistics
+                        {
+                            GameStarted = DateTime.Now.AddMinutes(-30),
+                            GameFinished = DateTime.Now.AddMinutes(-22),
                         }
                 };
+            SelectedGameStatistics = GameStatisticsHistory[0];
+
             PlayerList = new List<string>
                 {
                     "Player1",
@@ -373,23 +427,23 @@ namespace TetriNET.WPF_WCF_Client.ViewModels.Statistics
                             IsReadOnly = true,
                         },
                 };
-            List<SpecialRow> rows = new List<SpecialRow>();
+            List<SpecialStatisticsRow> rows = new List<SpecialStatisticsRow>();
             Random random = new Random();
-            foreach(Specials special in EnumHelper.GetSpecials(b => b))
+            foreach (Specials special in EnumHelper.GetSpecials(b => b))
             {
-                SpecialRow row = new SpecialRow
+                SpecialStatisticsRow row = new SpecialStatisticsRow
                     {
                         Special = special
                     };
-                foreach(DynamicColumn column in columns)
+                foreach (DynamicColumn column in columns)
                 {
                     int value = random.Next(20);
                     row.TryAddProperty(column.Name, value);
                 }
                 rows.Add(row);
             }
-            SpecialsFromSelectedPlayerGrid = new DynamicGrid<SpecialRow, DynamicColumn>(rows, columns);
-            SpecialsToSelectedPlayerGrid = new DynamicGrid<SpecialRow, DynamicColumn>(rows, columns);
+            SpecialsFromSelectedPlayerGrid = new DynamicGrid<SpecialStatisticsRow, DynamicColumn>(rows, columns);
+            SpecialsToSelectedPlayerGrid = new DynamicGrid<SpecialStatisticsRow, DynamicColumn>(rows, columns);
         }
     }
 }
