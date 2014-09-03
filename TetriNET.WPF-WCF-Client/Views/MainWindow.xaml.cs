7344c6ffb943fff6ed5c4556ced2caa8e211463a
SinaC <heymbeeck.joel@gmail.com>
2014-09-03 12:48:09 +0200

    KeyDown/Up moved into MainWindow
    Sometimes, Tab key while playing was causing tab control to take focus

--- TetriNET.WPF-WCF-Client/Views/MainWindow.xaml.cs
+++ TetriNET.WPF-WCF-Client/Views/MainWindow.xaml.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Input;
+using TetriNET.Client.Interfaces;
+using TetriNET.Client.Strategy;
+using TetriNET.Common.Helpers;
+using TetriNET.WPF_WCF_Client.AI;
 using TetriNET.WPF_WCF_Client.Helpers;
 using TetriNET.WPF_WCF_Client.ViewModels;
+using TetriNET.WPF_WCF_Client.ViewModels.Options;
+using TetriNET.WPF_WCF_Client.ViewModels.PlayField;
 
 namespace TetriNET.WPF_WCF_Client.Views
 {
@@ -12,6 +19,10 @@ namespace TetriNET.WPF_WCF_Client.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GameController.GameController _controller;
+
+        public GenericBot Bot { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,7 +39,15 @@ namespace TetriNET.WPF_WCF_Client.Views
 
 
             if (!DesignMode.IsInDesignModeStatic)
-                DataContext = new MainWindowViewModel();
+            {
+                MainWindowViewModel vm = new MainWindowViewModel();
+                DataContext = vm;
+
+                vm.ClientChanged += OnClientChanged;
+
+                if (vm.Client != null)
+                    OnClientChanged(null, vm.Client);
+            }
         }
 
         #region UI Events
@@ -48,10 +67,119 @@ namespace TetriNET.WPF_WCF_Client.Views
             this.Close();
         }
 
+        private void GameView_KeyDown(object sender, KeyEventArgs e)
+        {
+            MainWindowViewModel vm = DataContext as MainWindowViewModel;
+            if (e.Key == Key.S)
+            {
+                if (vm != null)
+                    vm.Client.StartGame();
+            }
+            else if (e.Key == Key.T)
+            {
+                if (vm != null)
+                    vm.Client.StopGame();
+            }
+            else if (e.Key == Key.P)
+            {
+                if (vm != null)
+                    vm.Client.PauseGame();
+            }
+            else if (e.Key == Key.R)
+            {
+                if (vm != null)
+                    vm.Client.ResumeGame();
+            }
+            else if (e.Key == Key.A && Keyboard.Modifiers == (ModifierKeys.Control | ModifierKeys.Shift))
+            {
+                Bot.Activated = !Bot.Activated;
+            }
+            else if (e.Key == Key.Add)
+            {
+                Bot.SleepTime += 100;
+            }
+            else if (e.Key == Key.Subtract)
+            {
+                Bot.SleepTime -= 100;
+            }
+            else
+            {
+                Commands cmd = MapKeyToCommand(e.Key);
+                if (cmd != Commands.Invalid)
+                    _controller.KeyDown(cmd);
+            }
+            if (e.Key == Key.Tab)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void GameView_KeyUp(object sender, KeyEventArgs e)
+        {
+            Commands cmd = MapKeyToCommand(e.Key);
+            if (cmd != Commands.Invalid)
+                _controller.KeyUp(cmd);
+            if (e.Key == Key.Tab)
+            {
+                e.Handled = true;
+            }
+        }
+
+        #endregion
+
+        #region IClient events handler
+
+        private void OnGameStarted()
+        {
+            if (ClientOptionsViewModel.Instance.DropSensibilityViewModel.IsActivated)
+                _controller.RemoveSensibility(Commands.Drop);
+            else
+                _controller.AddSensibility(Commands.Drop, ClientOptionsViewModel.Instance.DropSensibilityViewModel.Value);
+            if (ClientOptionsViewModel.Instance.DownSensibilityViewModel.IsActivated)
+                _controller.RemoveSensibility(Commands.Down);
+            else
+                _controller.AddSensibility(Commands.Down, ClientOptionsViewModel.Instance.DownSensibilityViewModel.Value);
+            if (ClientOptionsViewModel.Instance.LeftSensibilityViewModel.IsActivated)
+                _controller.RemoveSensibility(Commands.Left);
+            else
+                _controller.AddSensibility(Commands.Left, ClientOptionsViewModel.Instance.LeftSensibilityViewModel.Value);
+            if (ClientOptionsViewModel.Instance.RightSensibilityViewModel.IsActivated)
+                _controller.RemoveSensibility(Commands.Right);
+            else
+                _controller.AddSensibility(Commands.Right, ClientOptionsViewModel.Instance.RightSensibilityViewModel.Value);
+        }
 
         #endregion
 
         #region Commands
+
+        private void OnClientChanged(IClient oldClient, IClient newClient)
+        {
+            // Remove old handlers
+            if (oldClient != null)
+            {
+                oldClient.GameStarted -= OnGameStarted;
+
+                _controller.Do(x => x.UnsubscribeFromClientEvents());
+                Bot.Do(x => x.UnsubscribeFromClientEvents());
+            }
+            
+            // Add new handlers
+            if (newClient != null)
+            {
+                newClient.GameStarted += OnGameStarted;
+                // And create controller + bot
+                _controller = new GameController.GameController(newClient);
+                //Bot = new GenericBot(newClient, new LuckyToiletOnePiece(), null);
+                Bot = new GenericBot(newClient, new AdvancedPierreDellacherieOnePiece(), new SinaCSpecials());
+                //Bot = new GenericBot(newClient, new ColinFaheyTwoPiece(), new SinaCSpecials());
+            }
+            else
+            {
+                _controller = null;
+                Bot = null;
+            }
+        }
         
         private void CloseCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
@@ -59,5 +187,53 @@ namespace TetriNET.WPF_WCF_Client.Views
         }
 
         #endregion
+
+        private static Commands MapKeyToCommand(Key key)
+        {
+            KeySettingViewModel keySetting = ClientOptionsViewModel.Instance.KeySettings.FirstOrDefault(x => x.Key == key);
+            if (keySetting != null)
+                return keySetting.Command;
+            switch (key)
+            {
+                case Key.H:
+                    return Commands.Hold;
+                case Key.Space:
+                    return Commands.Drop;
+                case Key.Down:
+                    return Commands.Down;
+                case Key.Left:
+                    return Commands.Left;
+                case Key.Right:
+                    return Commands.Right;
+                case Key.Up:
+                    return Commands.RotateCounterclockwise;
+                case Key.PageDown:
+                    return Commands.RotateClockwise;
+                case Key.D:
+                    return Commands.DiscardFirstSpecial;
+                case Key.NumPad1:
+                case Key.D1:
+                    return Commands.UseSpecialOn1;
+                case Key.NumPad2:
+                case Key.D2:
+                    return Commands.UseSpecialOn2;
+                case Key.NumPad3:
+                case Key.D3:
+                    return Commands.UseSpecialOn3;
+                case Key.NumPad4:
+                case Key.D4:
+                    return Commands.UseSpecialOn4;
+                case Key.NumPad5:
+                case Key.D5:
+                    return Commands.UseSpecialOn5;
+                case Key.NumPad6:
+                case Key.D6:
+                    return Commands.UseSpecialOn6;
+                case Key.Enter:
+                    return Commands.UseSpecialOn6;
+            }
+            return Commands.Invalid;
+        }
+
     }
 }
--- TetriNET.WPF-WCF-Client/Views/PlayField/PlayFieldView.xaml.cs
+++ TetriNET.WPF-WCF-Client/Views/PlayField/PlayFieldView.xaml.cs
@@ -24,146 +24,12 @@ namespace TetriNET.WPF_WCF_Client.Views.PlayField
         {
             InitializeComponent();
 
-            //http://stackoverflow.com/questions/15241118/keydown-event-not-raising-from-a-grid
-            Loaded += (sender, args) =>
-            {
-                Focusable = true; // This is needed to catch KeyUp/KeyDown
-                Focus();
-            }; // This is needed to catch KeyUp/KeyDown
-        }
-
-        #region IClient events handler
-
-        private void OnGameStarted()
-        {
-            if (ClientOptionsViewModel.Instance.DropSensibilityViewModel.IsActivated)
-                _controller.RemoveSensibility(Commands.Drop);
-            else
-                _controller.AddSensibility(Commands.Drop, ClientOptionsViewModel.Instance.DropSensibilityViewModel.Value);
-            if (ClientOptionsViewModel.Instance.DownSensibilityViewModel.IsActivated)
-                _controller.RemoveSensibility(Commands.Down);
-            else
-                _controller.AddSensibility(Commands.Down, ClientOptionsViewModel.Instance.DownSensibilityViewModel.Value);
-            if (ClientOptionsViewModel.Instance.LeftSensibilityViewModel.IsActivated)
-                _controller.RemoveSensibility(Commands.Left);
-            else
-                _controller.AddSensibility(Commands.Left, ClientOptionsViewModel.Instance.LeftSensibilityViewModel.Value);
-            if (ClientOptionsViewModel.Instance.RightSensibilityViewModel.IsActivated)
-                _controller.RemoveSensibility(Commands.Right);
-            else
-                _controller.AddSensibility(Commands.Right, ClientOptionsViewModel.Instance.RightSensibilityViewModel.Value);
-        }
-
-        #endregion
-
-        #region UI events handler
-
-        private void GameView_KeyDown(object sender, KeyEventArgs e)
-        {
-            PlayFieldViewModel vm = DataContext as PlayFieldViewModel;
-            if (e.Key == Key.S)
-            {
-                if (vm != null)
-                    vm.Client.StartGame();
-            }
-            else if (e.Key == Key.T)
-            {
-                if (vm != null)
-                    vm.Client.StopGame();
-            }
-            else if (e.Key == Key.P)
-            {
-                if (vm != null)
-                    vm.Client.PauseGame();
-            }
-            else if (e.Key == Key.R)
-            {
-                if (vm != null)
-                    vm.Client.ResumeGame();
-            }
-            else if (e.Key == Key.A && Keyboard.Modifiers == (ModifierKeys.Control | ModifierKeys.Shift))
-            {
-                Bot.Activated = !Bot.Activated;
-            }
-            else if (e.Key == Key.H && Keyboard.Modifiers == (ModifierKeys.Control | ModifierKeys.Shift))
-            {
-                PlayerGrid.ToggleHint();
-                Inventory.ToggleHint();
-            }
-            //else if (e.Key == Key.D && Keyboard.Modifiers == (ModifierKeys.Control | ModifierKeys.Shift))
+            ////http://stackoverflow.com/questions/15241118/keydown-event-not-raising-from-a-grid
+            //Loaded += (sender, args) =>
             //{
-            //    PlayerGrid.ToggleDropLocation();
-            //}
-            else if (e.Key == Key.Add)
-            {
-                Bot.SleepTime += 100;
-            }
-            else if (e.Key == Key.Subtract)
-            {
-                Bot.SleepTime -= 100;
-            }
-            else
-            {
-                Commands cmd = MapKeyToCommand(e.Key);
-                if (cmd != Commands.Invalid)
-                    _controller.KeyDown(cmd);
-            }
-        }
-
-        private void GameView_KeyUp(object sender, KeyEventArgs e)
-        {
-            Commands cmd = MapKeyToCommand(e.Key);
-            if (cmd != Commands.Invalid)
-                _controller.KeyUp(cmd);
-        }
-        
-        #endregion
-
-        private Commands MapKeyToCommand(Key key)
-        {
-            KeySettingViewModel keySetting = ClientOptionsViewModel.Instance.KeySettings.FirstOrDefault(x => x.Key == key);
-            if (keySetting != null)
-                return keySetting.Command;
-            switch (key)
-            {
-                case Key.H:
-                    return Commands.Hold;
-                case Key.Space:
-                    return Commands.Drop;
-                case Key.Down:
-                    return Commands.Down;
-                case Key.Left:
-                    return Commands.Left;
-                case Key.Right:
-                    return Commands.Right;
-                case Key.Up:
-                    return Commands.RotateCounterclockwise;
-                case Key.PageDown:
-                    return Commands.RotateClockwise;
-                case Key.D:
-                    return Commands.DiscardFirstSpecial;
-                case Key.NumPad1:
-                case Key.D1:
-                    return Commands.UseSpecialOn1;
-                case Key.NumPad2:
-                case Key.D2:
-                    return Commands.UseSpecialOn2;
-                case Key.NumPad3:
-                case Key.D3:
-                    return Commands.UseSpecialOn3;
-                case Key.NumPad4:
-                case Key.D4:
-                    return Commands.UseSpecialOn4;
-                case Key.NumPad5:
-                case Key.D5:
-                    return Commands.UseSpecialOn5;
-                case Key.NumPad6:
-                case Key.D6:
-                    return Commands.UseSpecialOn6;
-                case Key.Enter:
-                    return Commands.UseSpecialOn6;
-            }
-            return Commands.Invalid;
+            //    Focusable = true; // This is needed to catch KeyUp/KeyDown
+            //    Focus();
+            //}; // This is needed to catch KeyUp/KeyDown
         }
 
         private void PlayFieldView_OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
@@ -181,33 +47,10 @@ namespace TetriNET.WPF_WCF_Client.Views.PlayField
 
         private void OnClientChanged(IClient oldClient, IClient newClient)
         {
-            // Remove old handlers
-            if (oldClient != null)
-            {
-                oldClient.GameStarted -= OnGameStarted;
-
-                _controller.Do(x => x.UnsubscribeFromClientEvents());
-                Bot.Do(x => x.UnsubscribeFromClientEvents());
-            }
             // Set new client
             Inventory.Client = newClient;
             NextPiece.Client = newClient;
             HoldPiece.Client = newClient;
-            // Add new handlers
-            if (newClient != null)
-            {
-                newClient.GameStarted += OnGameStarted;
-                // And create controller + bot
-                _controller = new GameController.GameController(newClient);
-                //Bot = new GenericBot(newClient, new LuckyToiletOnePiece(), null);
-                Bot = new GenericBot(newClient, new AdvancedPierreDellacherieOnePiece(), new SinaCSpecials());
-                //Bot = new GenericBot(newClient, new ColinFaheyTwoPiece(), new SinaCSpecials());
-            }
-            else
-            {
-                _controller = null;
-                Bot = null;
-            }
         }
     }
 }
